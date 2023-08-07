a00fe314c76f1ce719e66be776f8c484657b76f9
Ndibe Raymond Olisaemeka <rolisaemeka-ctr@wikimedia.org>
2023-08-07 15:25:20 +0100

    [builds-api]: Add build cancel endpoint
    Bug: T343606
    Signed-off-by: Ndibe Raymond Olisaemeka <rolisaemeka-ctr@wikimedia.org>

--- internal/build_test.go
+++ internal/build_test.go
@@ -705,3 +705,152 @@ func TestListReturnsBuilds(t *testing.T) {
 		t.Fatalf("Got unexpected build id, expected '%s', got '%s'", expectedBuildId, *gottenBuilds[0].BuildId)
 	}
 }
+
+func TestCancelReturnsErrorIfNotAllowed(t *testing.T) {
+	api := BuildsApi{
+		Clients: Clients{},
+		Config: Config{
+			BuildIdPrefix:  BuildIdPrefix,
+			BuildNamespace: BuildNamespace,
+		},
+	}
+
+	code, _ := Cancel(
+		&api,
+		"dummy-build-id",
+		"dummy-tool-name",
+	)
+
+	if code != 401 {
+		t.Fatalf("I was expecting a 401 response, got: %d", code)
+	}
+}
+
+func TestCancelReturnsNotFoundIfNoBuildsThere(t *testing.T) {
+	toolName := "dummy-tool"
+	buildId := fmt.Sprintf("%s%sbuild", toolName, BuildIdPrefix)
+	mockTekton := tektonFake.NewSimpleClientset(
+		&v1beta1.PipelineRunList{
+			Items: make([]v1beta1.PipelineRun, 0),
+		},
+	)
+	api := BuildsApi{
+		Clients: Clients{
+			Tekton: mockTekton,
+		},
+		Config: Config{
+			BuildIdPrefix:  BuildIdPrefix,
+			BuildNamespace: BuildNamespace,
+		},
+	}
+
+	code, _ := Cancel(
+		&api,
+		buildId,
+		toolName,
+	)
+
+	if code != 404 {
+		t.Fatalf("I was expecting a 404 response, got: %d", code)
+	}
+}
+
+func TestCancelReturnsInternalServerErrorOnException(t *testing.T) {
+	toolName := "dummy-tool"
+	buildId := fmt.Sprintf("%s%sbuild", toolName, BuildIdPrefix)
+	mockTekton := tektonFake.Clientset{}
+	fakePipelineRun := v1beta1.PipelineRun{
+		ObjectMeta: v1.ObjectMeta{Name: buildId, Namespace: BuildNamespace, Labels: map[string]string{"user": toolName}},
+	}
+
+	mockTekton.Fake.AddReactor(
+		"get",
+		"pipelineruns",
+		func(action k8sTesting.Action) (handled bool, ret k8sRuntime.Object, err error) {
+			return true, nil, fmt.Errorf("dummy error")
+		},
+	)
+	api := BuildsApi{
+		Clients: Clients{
+			Tekton: &mockTekton,
+		},
+		Config: Config{
+			BuildIdPrefix:  BuildIdPrefix,
+			BuildNamespace: BuildNamespace,
+		},
+	}
+	code, _ := Cancel(&api, buildId, toolName)
+	if code != 500 {
+		t.Fatalf("I was expecting a 500 response, got: %d", code)
+	}
+
+	mockTekton.Fake.PrependReactor(
+		"get",
+		"pipelineruns",
+		func(action k8sTesting.Action) (handled bool, ret k8sRuntime.Object, err error) {
+			return true, &fakePipelineRun, nil
+		},
+	)
+	mockTekton.Fake.AddReactor(
+		"update",
+		"pipelineruns",
+		func(action k8sTesting.Action) (handled bool, ret k8sRuntime.Object, err error) {
+			return true, nil, fmt.Errorf("dummy error")
+		},
+	)
+	api.Clients.Tekton = &mockTekton
+	code, _ = Cancel(&api, buildId, toolName)
+	if code != 500 {
+		t.Fatalf("I was expecting a 500 response, got: %d", code)
+	}
+}
+
+func TestCancelReturnsCancelledBuild(t *testing.T) {
+	toolName := "dummy-tool"
+	buildId := fmt.Sprintf("%s%sbuild", toolName, BuildIdPrefix)
+	mockTekton := tektonFake.NewSimpleClientset(
+		&v1beta1.PipelineRunList{
+			Items: []v1beta1.PipelineRun{
+				{
+					ObjectMeta: v1.ObjectMeta{Name: buildId, Namespace: BuildNamespace, Labels: map[string]string{"user": toolName}},
+					Spec: v1beta1.PipelineRunSpec{
+						Params: []v1beta1.Param{
+							{Name: "BUILDER_IMAGE", Value: v1beta1.ArrayOrString{Type: v1beta1.ParamTypeString, StringVal: "toolsbeta-harbor.wmcloud.org/toolforge/heroku-builder-classic:22"}},
+							{Name: "APP_IMAGE", Value: v1beta1.ArrayOrString{Type: v1beta1.ParamTypeString, StringVal: "192.168.188.129/tool-minikube-user/tool-raymond:latest"}},
+							{Name: "SOURCE_URL", Value: v1beta1.ArrayOrString{Type: v1beta1.ParamTypeString, StringVal: "https://github.com/david-caro/wm-lol"}},
+							{Name: "SOURCE_REFERENCE", Value: v1beta1.ArrayOrString{Type: v1beta1.ParamTypeString, StringVal: "value4"}},
+						},
+					},
+					Status: v1beta1.PipelineRunStatus{
+						Status: knative.Status{Conditions: knative.Conditions{{Reason: "Running", Status: "Unknown"}}},
+						PipelineRunStatusFields: v1beta1.PipelineRunStatusFields{
+							StartTime: &v1.Time{Time: time.Date(2023, 6, 8, 15, 0, 0, 0, time.UTC)},
+						},
+					},
+				},
+			},
+		},
+	)
+
+	api := BuildsApi{
+		Clients: Clients{
+			Tekton: mockTekton,
+		},
+		Config: Config{
+			BuildIdPrefix:  BuildIdPrefix,
+			BuildNamespace: BuildNamespace,
+		},
+	}
+	code, response := Cancel(&api, buildId, toolName)
+	if code != 200 {
+		t.Fatalf("I was expecting a 200 response, got: %d", code)
+	}
+
+	gottenBuild := response.(gen.Build)
+	if *gottenBuild.BuildId != buildId {
+		t.Fatalf("Got unexpected build id, expected '%s', got '%s'", buildId, *gottenBuild.BuildId)
+	}
+	if *gottenBuild.Status != "BUILD_CANCELLED" {
+		t.Fatalf("Got unexpected build status, expected 'cancelled', got '%s'", *gottenBuild.Status)
+	}
+}
